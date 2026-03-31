7c56f94cbbbd8ee25e95c7fd9dd9c974480759c3
LagerholmDani <lagerhod@arcada.fi>
2026-03-31 12:16:32 +0300

    ip update

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return { "msg": "Hello dev docker!" }
 
-
 @app.get("/hello")
 def hello():
     return { "msg": "Hello du!" }
+
+
+@app.get("/api/ip")
+def get_ip(request: Request):
+    return {"ip": request.client.host}
+
+
+@app.get("/ip", response_class=HTMLResponse)
+def get_html_ip(request: Request):
+    return f"<h1>Din publika IP-adress är {request.client.host}</h1>"
